87f73011518c24ae0bffc703dee3746ac7e503a7
3nelsonjunior <3nelsonjunior@gmail.com>
2017-10-18 15:36:20 -0200

    Voltando a versão normal após teste edição no topo.component.ts - AULA 40

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core' //importando recursos do node_modules
 
 @Component({
     selector:'app-topo',
-    //templateUrl: './topo.component.html'
-    template: '<p>  Este é o componente TOPO!!!! </p>' // definição template diretamente na associação
+    templateUrl: './topo.component.html'
+    
 })
 export class TopoComponent {} //declarando classe

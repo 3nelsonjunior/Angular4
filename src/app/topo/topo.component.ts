d1b395271b38d6ab1108594f0361fa2350698161
3nelsonjunior <3nelsonjunior@gmail.com>
2017-10-18 15:11:36 -0200

    incluindo topocomponent

---
+++ src/app/topo/topo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core' //importando recursos do node_modules
+
+@Component({
+    selector:'app-topo',
+    templateUrl: './topo.component.html'
+    
+})
+export class TopoComponent {} //declarando classe

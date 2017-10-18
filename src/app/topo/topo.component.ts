56d1985bce9287cc8b7704aab39498701e2a4048
3nelsonjunior <3nelsonjunior@gmail.com>
2017-10-18 15:58:42 -0200

    Adicionando topo.component.css - AULA 41

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core' //importando recursos do node_modules
 
 @Component({
     selector:'app-topo',
-    templateUrl: './topo.component.html'
-    
+    templateUrl: './topo.component.html',
+    styleUrls: ['./topo.component.css']
 })
 export class TopoComponent {} //declarando classe

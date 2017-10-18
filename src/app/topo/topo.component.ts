1b7d97fcc000e4c3562102f8ed785d6b61d05167
3nelsonjunior <3nelsonjunior@gmail.com>
2017-10-18 15:17:16 -0200

    edição no topo.component.ts - AULA 40

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core' //importando recursos do node_modules
 
 @Component({
     selector:'app-topo',
-    templateUrl: './topo.component.html'
-    
+    //templateUrl: './topo.component.html'
+    template: '<p>  Este é o componente TOPO!!!! </p>' // definição template diretamente na associação
 })
 export class TopoComponent {} //declarando classe

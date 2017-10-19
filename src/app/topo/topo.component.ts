e3dfedb1bde7ae1de45cf966d4cf82a67dcb0b32
3nelsonjunior <3nelsonjunior@gmail.com>
2017-10-19 13:53:03 -0200

    AULA 51 - String interpolation / DATA BINDING

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -5,4 +5,8 @@ import { Component } from '@angular/core' //importando recursos do node_modules
     templateUrl: './topo.component.html',
     styleUrls: ['./topo.component.css']
 })
-export class TopoComponent {} //declarando classe
+export class TopoComponent {
+    //data binding
+    //classe: criandor atributo para interpolação(para inserção no template)
+    public titulo: string = 'Aprendendo Inglês'    
+} 

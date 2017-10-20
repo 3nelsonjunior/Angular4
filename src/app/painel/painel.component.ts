9a6c01a18ad792724cedd37feba03f59d9bf5449
3nelsonjunior <3nelsonjunior@gmail.com>
2017-10-20 09:18:51 -0200

    AULA 63 - Estabelecendo a lógica das rodadas de frases

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -11,15 +11,29 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES
   public instrucao: string = 'Traduza a frase: '
+  public resposta: string
 
-  constructor() { console.log(this.frases)}
+  //variaver para armazenar a rodada atual
+  public rodada: number = 0;
+  //variavel para armazenar Frase atual
+  public rodadaFrase: Frase
+
+  constructor() { 
+    //inicializando objeto com a primeira frase
+    this.rodadaFrase = this.frases[this.rodada]    
+    console.log(this.rodadaFrase)
+  }
 
   ngOnInit() {
   }
 
   //função para evento ao digitar no textarea
-  public atualizaResposta(): void{
-    console.log('teste')
+  public atualizaResposta(resposta: Event): void{
+    // event binding - pegando dados do HTML via entrada de dados do usuario
+    console.log((<HTMLInputElement>resposta.target).value)
+    console.log(this.resposta)
   }
+
+  
 
 }
